c00af08d8a9547e40fd63b33069caa9040105923
annakokorina <60149858+annakokorina@users.noreply.github.com>
2023-06-07 16:46:02 +0400

    MailService.java

--- MailService.java
+++ MailService.java
@@ -7,32 +7,37 @@ import java.util.function.Consumer;
 
 public class MailService<T> implements Consumer<Mail<T>> {
 
-    private static class MyMap<K,V> extends HashMap<K,V>{
-        @Override
-        public V get(Object key) {
-            if (super.get(key) == null) {
-                return (V) Collections.<String>emptyList();
-            }
-            else return super.get(key);
-        }
+    Map<String, List<T>> mailBox;
+
+    public MailService() {
+        mailBox = new MyMap<>();
     }
-    
-    Map<String, List<T>> mailBox = new HashMap<>();
+
     public Map<String, List<T>> getMailBox() {
         return mailBox;
     }
 
     @Override
     public void accept(Mail<T> tMail) {
-        
-        if (!this.mailBox.containsKey(tMail.getTo())) {
-            List<T> value = new ArrayList<>();
-            value.add(tMail.getContent());
-            this.mailBox.put(tMail.getTo(), value);
-        } else {
+
+        if (this.mailBox.containsKey(tMail.getTo())) {
             List<T> value = this.mailBox.get(tMail.getTo());
             value.add(tMail.getContent());
             this.mailBox.put(tMail.getTo(), value);
+        } else {
+            List<T> value = new ArrayList<>();
+            value.add(tMail.getContent());
+            this.mailBox.put(tMail.getTo(), value);
+        }
+    }
+
+    public static class MyMap<K,V> extends HashMap<K,V>{
+        @Override
+        public V get(Object key) {
+            if (super.get(key) == null) {
+                return (V) Collections.<String>emptyList();
+            }
+            return super.get(key);
         }
     }
 }
